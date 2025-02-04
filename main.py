2c5522ee5daab9288662058ab11a864bb11977d7
desmili <desai.milind@gmail.com>
2025-02-04 13:32:46 +0530

    Update main.py
    Added missing api_key into openai()

--- main.py
+++ main.py
@@ -24,15 +24,10 @@ app.add_middleware(
 
 #MilindKey
 #client = openai.OpenAI(api_key="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")
-
-
-
 #MMDKey
 #client = openai.OpenAI(api_key="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")
-
 #DiagnoBotKey
-client = openai.OpenAI("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")
-
+client = openai.OpenAI(api_key="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")
 
 #print(response.choices[0].message.content)
 # Medical terms for validation
