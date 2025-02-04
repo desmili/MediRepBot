90ffbaa4093dd3cea162c7f02cdbf10de68b95d1
desmili <desai.milind@gmail.com>
2025-02-04 12:57:11 +0530

    Update main.py
    Changed to gpt-4o-mini

--- main.py
+++ main.py
@@ -60,9 +60,9 @@ def is_medical_report(text):
 
 def summarize_text(text):
     """Summarize the medical report using OpenAI"""
-    #gpt-3.5-turbo, gpt-4, gpt-4-turbo
+    #gpt-3.5-turbo, gpt-4, gpt-4-turbo, gpt-4o-mini
     response = client.chat.completions.create(
-        model="gpt-3.5-turbo", 
+        model="gpt-4o-mini", 
             messages=[
             {"role": "system", "content": "You are a medical assistant that simplifies reports for patients."},
             {"role": "user", "content": f"Summarize this medical report in layman's terms:\n{text}"}
